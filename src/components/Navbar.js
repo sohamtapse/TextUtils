acc4295e049f99ab79a04df0c3ab777d1508b1d5
sohamtapse <tapsesoham0@gmail.com>
2024-05-13 15:08:01 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link} from "react-router-dom";
-import { Router } from "react-router-dom";
+// import { Link} from "react-router-dom";
+// import { Router } from "react-router-dom";
 
 
 export default function Navbar(props) {
@@ -11,24 +11,24 @@ export default function Navbar(props) {
     >
       <div className="container-fluid">
         
-        <Link className="navbar-brand" to="/">
+        <a className="navbar-brand" href="#">
           {props.Title}
-        </Link>
+        </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav  mb-2 mb-lg-0 mx-0">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/home">
+              <a className="nav-link active" aria-current="page" href="#">
                 Home
-              </Link>
+              </a>
             </li>
-            <li className="nav-item">
+            {/* <li className="nav-item">
               <Link className="nav-link active" aria-current="page" to="/about">
                 About
               </Link>
-            </li>
+            </li> */}
           </ul>
           {/* <form className="d-flex me-auhref" role="search">
             <input
